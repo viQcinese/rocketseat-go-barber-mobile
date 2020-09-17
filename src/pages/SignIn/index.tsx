ab688dee32596ec98b85bc60ef09e94129178e95
Vitor Senise <vitor.senise@gmail.com>
2020-09-17 03:02:26 -0300

    Navigation between SignIn and SignUp. View avoiding keyboard

--- src/pages/SignIn/index.tsx
+++ src/pages/SignIn/index.tsx
@@ -1,30 +1,85 @@
-import React from 'react';
-import { Image } from 'react-native';
+import React, { useState, useEffect } from 'react';
+import {
+  Image,
+  KeyboardAvoidingView,
+  ScrollView,
+  Platform,
+  View,
+  Keyboard,
+} from 'react-native';
+import Icon from 'react-native-vector-icons/Feather';
+import { useNavigation } from '@react-navigation/native';
 
 import Input from '../../components/Input';
 import Button from '../../components/Button';
 
 import logoImg from '../../assets/logo.png';
 
-import { Container, Title } from './styles';
+import {
+  Container,
+  Title,
+  ForgotPassword,
+  ForgotPasswordText,
+  CreateAccountButton,
+  CreateAccountButtonText,
+} from './styles';
 
-const SignIn: React.FC = () => (
-  <Container>
-    <Image source={logoImg} />
+const SignIn: React.FC = () => {
+  const navigation = useNavigation();
 
-    <Title>Faça seu logon</Title>
+  const [isKeyboardOpen, setIsKeyboardOpen] = useState(false);
 
-    <Input name="email" icon="mail" placeholder="email" />
-    <Input name="password" icon="lock" placeholder="senha" />
+  useEffect(() => {
+    Keyboard.addListener('keyboardDidShow', e => setIsKeyboardOpen(true));
+    Keyboard.addListener('keyboardDidHide', e => setIsKeyboardOpen(false));
 
-    <Button
-      onPress={() => {
-        console.log('hi');
-      }}
-    >
-      Entrar
-    </Button>
-  </Container>
-);
+    return () => {
+      Keyboard.removeAllListeners('keyboardDidShow');
+      Keyboard.removeAllListeners('keyboardDidHide');
+    };
+  }, []);
+
+  return (
+    <>
+      <KeyboardAvoidingView
+        style={{ flex: 1 }}
+        behavior={Platform.OS === 'ios' ? 'padding' : undefined}
+        enabled
+      >
+        <ScrollView>
+          <Container>
+            <Image source={logoImg} />
+
+            <View>
+              <Title>Faça seu logon</Title>
+            </View>
+
+            <Input name="email" icon="mail" placeholder="email" />
+            <Input name="password" icon="lock" placeholder="senha" />
+
+            <Button
+              onPress={() => {
+                console.log('hi');
+              }}
+            >
+              Entrar
+            </Button>
+
+            <ForgotPassword>
+              <ForgotPasswordText>Esqueci minha senha</ForgotPasswordText>
+            </ForgotPassword>
+          </Container>
+        </ScrollView>
+      </KeyboardAvoidingView>
+
+      {!isKeyboardOpen && (
+        <CreateAccountButton onPress={() => navigation.navigate('SignUp')}>
+          <Icon name="log-in" size={20} color="#ff9000" />
+          <CreateAccountButtonText>Criar uma conta</CreateAccountButtonText>
+        </CreateAccountButton>
+      )}
+    </>
+  );
+};
 
 export default SignIn;
--- src/pages/SignUp/index.tsx
+++ src/pages/SignUp/index.tsx
@@ -1,12 +1,75 @@
-import React from 'react';
-import { Text } from 'react-native';
+import React, { useState, useEffect } from 'react';
+import {
+  Image,
+  KeyboardAvoidingView,
+  ScrollView,
+  Platform,
+  View,
+  Keyboard,
+} from 'react-native';
+import Icon from 'react-native-vector-icons/Feather';
+import { useNavigation } from '@react-navigation/native';
 
-import { Container } from './styles';
+import Input from '../../components/Input';
+import Button from '../../components/Button';
 
-const SignUp: React.FC = () => (
-  <Container>
-    <Text>SignUp</Text>
-  </Container>
-);
+import logoImg from '../../assets/logo.png';
+
+import { Container, Title, BackToSignIn, BackToSignInText } from './styles';
+
+const SignUp: React.FC = () => {
+  const navigation = useNavigation();
+
+  const [isKeyboardOpen, setIsKeyboardOpen] = useState(false);
+
+  useEffect(() => {
+    Keyboard.addListener('keyboardDidShow', e => setIsKeyboardOpen(true));
+    Keyboard.addListener('keyboardDidHide', e => setIsKeyboardOpen(false));
+
+    return () => {
+      Keyboard.removeAllListeners('keyboardDidShow');
+      Keyboard.removeAllListeners('keyboardDidHide');
+    };
+  }, []);
+
+  return (
+    <>
+      <KeyboardAvoidingView
+        style={{ flex: 1 }}
+        behavior={Platform.OS === 'ios' ? 'padding' : undefined}
+        enabled
+      >
+        <ScrollView>
+          <Container>
+            <Image source={logoImg} />
+
+            <View>
+              <Title>Crie sua conta</Title>
+            </View>
+
+            <Input name="nam" icon="user" placeholder="nome" />
+            <Input name="email" icon="mail" placeholder="email" />
+            <Input name="password" icon="lock" placeholder="senha" />
+
+            <Button
+              onPress={() => {
+                console.log('hi');
+              }}
+            >
+              Entrar
+            </Button>
+          </Container>
+        </ScrollView>
+      </KeyboardAvoidingView>
+
+      {!isKeyboardOpen && (
+        <BackToSignIn onPress={() => navigation.navigate('SignIn')}>
+          <Icon name="arrow-left" size={20} color="#fff" />
+          <BackToSignInText>Voltar para logon</BackToSignInText>
+        </BackToSignIn>
+      )}
+    </>
+  );
+};
 
 export default SignUp;
